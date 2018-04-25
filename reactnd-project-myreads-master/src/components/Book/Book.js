bc6bf7ea46f81d91b3b2de277db9bfd809f16bcd
Clementine Boulet <cleboule@cobogpgp7255.global.publicisgroupe.net>
2018-04-24 22:52:10 -0500

    refactor: basic refactor over starting code
    separate the app into different components book and bookshelf for a better vison and reutilisation of the code

---
+++ reactnd-project-myreads-master/src/components/Book/Book.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Book.css'
+import locales from '../../locales/en-US'
+
+/**
+* @description Represents a book
+* @constructor
+* @param {string} title - The title of the book
+* @param {string} author - The author of the book
+* @param {string} imageUrl - The image url of the book
+*/
+
+const Book = ({
+  title, author, imageUrl, ...others
+}) => {
+  const { bookOption, bookShelf } = locales
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageUrl})` }} />
+        <div className="book-shelf-changer">
+          <select>
+            {
+              Object.keys(bookOption).map(key => (
+                <option value={key} key={key} disabled>{bookOption[key]}</option>
+              ))
+            }
+            {
+              Object.keys(bookShelf).map(key => (
+                <option value={key} key={key}>{bookShelf[key]}</option>
+              ))
+            }
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{author}</div>
+    </div>
+  )
+}
+
+export default Book
---
+++ reactnd-project-myreads-master/src/locales/en-US.js
@@ -0,0 +1,13 @@
+const locales = {
+  bookOption: {
+    move: 'Move to...'
+  },
+  bookShelf: {
+    currentlyReading: 'Currently Reading',
+    wantToRead: 'Want to Read',
+    read: 'Read',
+    none: 'None'
+  }
+}
+
+export default locales
